e38aa84a936a7ae5f25a94b1fa0de18acdf9e643
Noamaan Mulla <noamaan.mulla03@gmail.com>
2025-11-19 13:48:12 +0530

    Add comments in user model.

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,29 +1,43 @@
+// import the query function from db.js
 import query from "../db";
 
+// user model object
 const userModel = {
+
+    // create a new user
     createUser: async (username, email, hashedPassword) => {
+        // create query text and parameters
         const queryText = "INSERT INTO users(username, email, password) VALUES($1, $2, $3) RETURNING id, username, email";
         const queryParams = [username, email, hashedPassword];
+
         try {
+            // execute the query
             const res = await query(queryText, queryParams);
             return res.rows[0];
         } catch (err) {
+            // handle errors
             console.error(`[-] Error creating user: ${err.message}`);
             throw err;
         }
     },
 
+    // find user by ID
     findUserById: async (id) => {
+        // create query text and parameters
         const queryText = "SELECT id, username, email FROM users WHERE id = $1";
         const queryParams = [id];
+
         try {
+            // execute the query
             const res = await query(queryText, queryParams);
             return res.rows[0];
         } catch (err) {
+            // handle errors
             console.error(`[-] Error finding user by ID: ${err.message}`);
             throw err;
         }
     },
 };
 
+// export the user model
 export default userModel;
