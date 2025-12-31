05e79c1ebbdc410d3bad92c7572342faeec892ea
Noamaan Mulla <noamaan.mulla03@gmail.com>
2025-12-31 16:11:42 +0530

    Minor bug

--- server/index.js
+++ server/index.js
@@ -45,7 +45,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Serve static files from uploads directory
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+app.use('/uploads', express.static(join(__dirname, 'uploads')));
 
 // CORS for frontend - must be before routes
 app.use((req, res, next) => {
